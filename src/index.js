585f8ab775e61f5f894dd065e2256f0b8dcf28f1
maxhannan <maxhannan@substanceseven.com>
2021-02-15 01:15:39 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import '../node_modules/@fortawesome/fontawesome-free/js/all';
+const element = document.createElement('div');
+element.innerHTML =  '<i class="fas fa-rocket fa-3x"></i><br>'
+document.body.appendChild(element)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
+module: {
+    loaders: [
+        {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+                }
+              }
+            ]
+          }
+        ]
+      }
