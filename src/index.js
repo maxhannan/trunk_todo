9e42f7f54c1c01857672cc99eb33d8ed13772a10
maxhannan <maxhannan@substanceseven.com>
2021-02-15 01:53:49 -0600

    initial layout/

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import '../node_modules/@fortawesome/fontawesome-free/js/all';
-const element = document.createElement('div');
-element.innerHTML =  '<i class="fas fa-rocket fa-3x"></i><br>'
-document.body.appendChild(element)
+const closer = document.querySelector('#closer')
+const sideBar = document.querySelector('.sideBar')
+const main = document.querySelector('.main')
+
+const toggleSide = () =>{
+    sideBar.classList.toggle('close')
+    main.classList.toggle('fullScreen')
+    closer.classList.toggle('closed')
+}
+closer.addEventListener('click', toggleSide)
