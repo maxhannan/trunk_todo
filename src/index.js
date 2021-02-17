bd094f17c6de832c54fd36ac567625c9a704c199
maxhannan <maxhannan@substanceseven.com>
2021-02-16 20:48:25 -0600

    let's do this one more time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import '../node_modules/@fortawesome/fontawesome-free/js/all';
-import * as dropdown from './dropdown';
 
-const opener = document.querySelector('#opener');
-const form = document.querySelector('#formy');
-
-
-const formTog = () =>{
-    form.classList.toggle('closed')
-}
-opener.addEventListener('click', formTog)
+alert('Blank Slate! Have some fun for christ sake')
