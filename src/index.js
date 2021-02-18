4f247518135f0e6096c54db8d49ce9c353623627
maxhannan <maxhannan@substanceseven.com>
2021-02-17 18:41:18 -0600

    initial commit redo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import '../node_modules/@fortawesome/fontawesome-free/js/all';
 
-alert('Blank Slate! Have some fun for christ sake')
+function handleOpenTask(e){
+ let target = e.target.closest('li');
+ let listItems = document.querySelectorAll('li');
+ listItems.forEach(li => {
+   if(li != target){
+     li.classList.remove('open'); 
+     li.querySelector('.openArrow').classList.remove('openBtnOpen') 
+   }
+  })
+ console.log(target)
+ e.target.closest('div').classList.toggle('openBtnOpen')
+
+ target.classList.toggle('open')
+}
+function handleChange(e){
+  console.log(e.target.value)
+}
+function  handleCheck(e){
+  e.target.classList.toggle('checked')
+  let hol = e.target.closest('.taskHolder')
+  
+  let task = hol.querySelector('.taskText')
+  task.classList.toggle('completed')
+}
+const openBtns = document.querySelectorAll('.openArrow')
+const inputs = document.querySelectorAll('input')
+const checkers = document.querySelectorAll('.checker')
+console.log(inputs)
+checkers.forEach(check => check.addEventListener('click', handleCheck))
+inputs.forEach(inp => inp.addEventListener('change', handleChange))
+openBtns.forEach(btn => btn.addEventListener('click', handleOpenTask))
